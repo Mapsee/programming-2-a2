0d34710daf691a5860bb036c484c451d5cad9926
Nicole Leung <mapsee26@gmail.com>
2020-12-09 11:43:34 -0800

    Update question to quiz.py

--- python f.py
+++ python f.py
@@ -1,44 +1,78 @@
 # quiz.py
 
+# Counter
 counter = 0
 
-x = float(input("Question 1: What is 4+5? "))
-if x == 9:
-    print("Good Job")
+# Introduction
+input("Hello welcome to the mini quiz! Are you ready to answer questions? ")
+print("Aight here we go! ")
+
+# Simple addition question
+number = input("\nQuestion 1: What is 4+5? ")
+if number == "9":
+    print(f"Good Job!")
     counter += 1
 else:
-    print("Sorry you got it wrong ;-;")
+    print(f"Sorry you got it wrong ;-;")
+print(f"{counter}/5")
 
-print("Question 2: What is the acceleration of gravity?\nA = 1.0m/s^2\nB = 3.5m/s^2\nC = 1.9m/s^2\nD = 9.8m/s^2")
-letter = input("Please input a letter: ").upper()
+# What is acceleration of gravity?
+print("\nQuestion 2: What is the acceleration of gravity?\nA = 1.0m/s^2\nB = 3.5m/s^2\nC = 1.9m/s^2\nD = 9.8m/s^2")
+letter = input("Please input a letter: ").upper().strip("?!@#$%^&()")
 if letter == "D":
-    print("Good job")
+    print("Yay you got it right. ")
     counter += 1
 else:
-    print("Bruh")
+    print("That wasn't the right answer .-.")
+print(f"{counter}/5")
 
-answer = input("What is Karen's last name? ")
-if answer == "Yao":
-    print("Yea she's hot i know")
-    counter+=1
+# Iron Man
+answer = input("\nQuestion 3: I love you ____ ")
+if answer == "3000":
+    print("Congrats you got it right! ")
+    counter += 1
 else:
-    "i don't like ur cut g"
+    print("Wrong answer. ")
+print(f"{counter}/5")
 
-yes = input("Give me two words that define a vector: ")
-physics = yes.lower().strip( )
-if physics == "displacementdirection":
-    print("Good job even I couldn't believe my eyes that your actually competent")
-    counter+=1
+# More physics
+print("\nQuestion 4: What are two words that define a vector? ")
+define1 = input("Enter the first word: ").lower().strip("!?@#$%^&*() ")
+define2 = input("Enter the second word: ").lower().strip("!?@#$%^&*() ")
+vector_definition = f"{define1}{define2}"
+if vector_definition == "displacementdirection" or vector_definition == "directiondisplacement":
+    print("Yay that's the correct answer! ")
+    counter += 1
 else:
-    print("tu es stupide")
+    print("Tu as tort.")
+print(f"{counter}/5\n")
 
-if counter == 1:
-    print("your iq low, you got 1/5")
-elif counter ==2:
-    print("could be better bro, 2/5")
+# What was the answer for question 1
+re_answer = input("Question 5: What was the answer for Question 1? ")
+if re_answer == "9":
+    print("Wow you have a good memory! Good job you got it right. ")
+    counter += 1
+else:
+    print("Incorrect. ")
+print(f"{counter}/5\n")
+
+# Rate the quiz
+rating = input("!!!ALERT BOSS FIGHT!!! Bonus Question: Did you enjoy the quiz? ")
+counter += 1
+print(f"Alright thanks bro\n{counter}/5")
+
+# Print results
+if counter == 0:
+    print("You failed :(, You got 0/5 in total")
+elif counter == 1:
+    print("At least you got a point yay! 1/5")
+elif counter == 2:
+    print("Could be better bro, 2/5")
 elif counter == 3:
-    print("ur passed, 3/5")
-elif counter ==4:
-    print("chicken nuggets, 4/5")
+    print("You passed! 3/5")
+elif counter == 4:
+    print("Chicken nuggets, 4/5")
+elif counter == 5:
+    print("Good job you got full marks! 5/5")
 else:
-    print("Good job you got 5/5 ")
+    print("Woooaaahhhh you scored 6/5, your brain big ")
