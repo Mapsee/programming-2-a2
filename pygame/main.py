2562927c19e47449c501f1336034a1bf73b5fded
Mapsee <mapsee26@gmail.com>
2021-01-20 14:55:19 -0800

    yes

--- pygame/main.py
+++ pygame/main.py
@@ -3,12 +3,16 @@
 # TODO: Make it so player can move diagonally without choppiness
 # TODO: Collision with block forces player to not move smoothly (Line 48)
 
+# testing the repository for VSC
+
 import pygame
 
 # ----- CONSTANTS
 BLACK = (0, 0, 0)
 SKY_BLUE = (95, 165, 228)
 GRAY = (134, 136, 138)
+GREEN = (65, 169, 76)
+RED = (255, 0, 0)
 
 WIDTH = 800
 HEIGHT = 800
@@ -46,6 +50,16 @@ class Player(pygame.sprite.Sprite):
 
         # See if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            # Restrict the block from moving upwards if it hits the bottom of platform
+            if self.vel_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.vel_y < 0:
+                self.rect.top = block.rect.bottom
+            elif self.vel_x > 0: 
+                self.rect.right = block.rect.left
+            elif self.vel_x < 0: 
+                self.rect.left = block.rect.right
 
 
     # Player movement
@@ -68,6 +82,17 @@ class Player(pygame.sprite.Sprite):
         self.vel_x = 0
         self.vel_y = 0
 
+class Goal(pygame.sprite.Sprite):
+    """End goal that the user must reach"""
+    def __init__(self):
+        super().__init__()
+
+        width = 40
+        height = 40
+        self.image = pygame.Surface([width, height])
+        self.image.fill(GREEN)
+
+        self.rect = self.image.get_rect()
 
 class Platform(pygame.sprite.Sprite):
     """Block that user cannot go through"""
@@ -78,6 +103,31 @@ class Platform(pygame.sprite.Sprite):
         self.image.fill(GRAY)
 
         self.rect = self.image.get_rect()
+
+class Enemies(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # image
+        self.image = pygame.Surface((20, 20))
+        self.image.fill(RED)
+
+        # rect
+        self.rect = self.image.get_rect()
+        self.x_vel = 3
+        self.y_vel = -3
+    def update(self):
+        """Move at random"""
+        # update x- and y- location of block based on x_vel and y_vel
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+
+        # bounce when it reaches the end of the screen
+        if self.rect.x < 0 or self.rect.x + self.rect.width > WIDTH:
+            self.x_vel *= -1
+
+        if self.rect.y < 0 or self.rect.y + self.rect.height > HEIGHT:
+            self.y_vel *= -1
 
 class Level(object):
     """Super-class used to define a level. Create a child class for level-specific info"""
@@ -110,8 +160,8 @@ class Level_01(Level):
         Level.__init__(self, player)
 
         # TODO: List the platforms here
-        level = [[210, 70, 500, 500],
-                 [210, 70, 200, 400],
+        level = [[50, 1600, 500, 500],
+                 [50, 1600, 200, 400],
                  [210, 70, 600, 300],
                  [100, 70, 1000, 100],
                  [150, 70, 1000, 300]
@@ -134,8 +184,10 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption(TITLE)
 
-    # Create the player
+    # Create the player and goal
     player = Player()
+    goal = Goal()
+    enemy = Enemies()
 
     # Create the levels
     level_list = []
@@ -146,12 +198,14 @@ def main():
     current_level = level_list[current_level_no]
 
     # ----- LOCAL VARIABLES
+    enemy_sprites = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
     player.level = current_level
 
     player.rect.x = 340
     player.rect.y = 100
     all_sprites.add(player)
+    all_sprites.add(goal)
 
     done = False
     clock = pygame.time.Clock()
@@ -189,6 +243,7 @@ def main():
 
         # ----- DRAW
         current_level.draw(screen)
+        enemy_sprites.draw(screen)
         all_sprites.draw(screen)
 
 
